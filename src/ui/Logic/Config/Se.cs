1ba407d51f0ddc34643612e4a8d10ff263c973a3
Nikolaj Olsson <nikse.dk@gmail.com>
2026-05-09 09:06:16 +0200

    Update version to v5.0.0-beta23

--- src/ui/Logic/Config/Se.cs
+++ src/ui/Logic/Config/Se.cs
@@ -185,7 +185,7 @@ public class Se
         return Path.Combine(TesseractFolder, "tessdata");
     }
 
-    public static string Version { get; set; } = "v5.0.0-beta22";
+    public static string Version { get; set; } = "v5.0.0-beta23";
 
     public void InitializeMainShortcuts(MainViewModel vm)
     {
